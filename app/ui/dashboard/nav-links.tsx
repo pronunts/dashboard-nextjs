babde2d81bcac5a942d2867791eb632acebc3df7
Jose Rodriguez <joserodriguezrafa@gmail.com>
2024-02-07 19:04:47 -0400

    2024

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -34,16 +34,13 @@ export default function NavLinks() {
             key={link.name}
             href={link.href}
             className={
-              clsx(
-              "flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3"
-              ,
-              {
-              'bg-sky-100 text-blue-600': pathname === link.href,
-              }
-             /*  {
-                'text-red-500 bg-black' : pathname !== link.href,  Esto es solo para probar las condicionales de next
-              } */
-            )}
+              `flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3
+              ${pathname === link.href ? 'text-blue-600 bg-sky-100' : ''}
+              `
+              /*  {
+                 'text-red-500 bg-black' : pathname !== link.href,  Esto es solo para probar las condicionales de next
+               } */
+            }
           >
             <LinkIcon className="w-6" />
             <p className="hidden md:block">{link.name}</p>
